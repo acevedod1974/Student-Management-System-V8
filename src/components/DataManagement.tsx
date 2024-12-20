b0a0d7439be96a79a9eb72fa16dd656643a22396
Daniel Acevedo Lopez <93848188+acevedod1974@users.noreply.github.com>
2024-12-20 19:26:18 -0400

    fix(DataManagement): update Azure storage connection string to use shared access signature

--- src/components/DataManagement.tsx
+++ src/components/DataManagement.tsx
@@ -28,7 +28,7 @@ import toast from "react-hot-toast";
 import { BlobServiceClient } from "@azure/storage-blob";
 
 const AZURE_STORAGE_CONNECTION_STRING =
-  "DefaultEndpointsProtocol=https;AccountName=sms8;AccountKey=3bCED08qpg8aCRB9fTFj0OUGEdPhou5uzmP4/SBgz//PYwetllT8DCFFLuKMOIw2yeDKVlg2WjQf+AStCy5RzA==;EndpointSuffix=core.windows.net";
+  "BlobEndpoint=https://sms8.blob.core.windows.net/;QueueEndpoint=https://sms8.queue.core.windows.net/;FileEndpoint=https://sms8.file.core.windows.net/;TableEndpoint=https://sms8.table.core.windows.net/;SharedAccessSignature=sv=2022-11-02&ss=bfqt&srt=co&sp=rwdlacupiytfx&se=2025-01-21T07:24:03Z&st=2024-12-20T23:24:03Z&spr=https&sig=CRZFhnlOCRTp9MZs8JvT8YNpmn5Z8zxGs%2FC6NuOMhGQ%3D";
 const CONTAINER_NAME = "backups";
 
 export const DataManagement: React.FC = () => {

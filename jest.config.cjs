67c76135037e49f5db4e8861eae2141e99e36cfb
Daniel Acevedo Lopez <93848188+acevedod1974@users.noreply.github.com>
2024-12-20 01:49:51 -0400

    refactor(jest.config): add JUnit reporter with timestamped output for test results

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,18 @@
+const { format } = require("date-fns");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testMatch: ["<rootDir>/src/tests/**/*.test.tsx"],
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "logs",
+        outputName: `junit-${format(new Date(), "yyyy-MM-dd-HH-mm-ss")}.xml`,
+      },
+    ],
+  ],
 };

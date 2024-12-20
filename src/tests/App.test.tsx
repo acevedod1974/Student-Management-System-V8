b5e63455b5f83e8d8e4906b9041028d862b46e8e
Daniel Acevedo Lopez <93848188+acevedod1974@users.noreply.github.com>
2024-12-20 01:06:24 -0400

    remove unused jest-dom import from App test file

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
-import "@testing-library/jest-dom/extend-expect";
 
 test("renders learn react link", () => {
   render(<App />);

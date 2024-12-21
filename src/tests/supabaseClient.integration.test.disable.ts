8dc668d894803f5677a57d67332f5b26fffeeb27
Daniel Acevedo Lopez <93848188+acevedod1974@users.noreply.github.com>
2024-12-20 20:40:55 -0400

    refactor(tests): simplify Supabase client integration tests by removing unused variables and using environment variables for configuration

--- src/tests/supabaseClient.integration.test.disable.ts
+++ src/tests/supabaseClient.integration.test.disable.ts
@@ -20,36 +20,24 @@ afterAll(async () => {
 describe("Supabase Client Integration", () => {
   it("should insert and retrieve data from the test_table", async () => {
     // Insert data
-    const {
-      error: insertError,
-      status: insertStatus,
-      statusText: insertStatusText,
-    } = await supabase
+    const { error: insertError, status: insertStatus } = await supabase
       .from("test_table")
       .insert([{ name: "Test User", email: "test@example.com" }]);
 
     console.log("Insert Error:", insertError);
     console.log("Insert Status:", insertStatus);
-    console.log("Insert Status Text:", insertStatusText);
 
     expect(insertError).toBeNull();
     expect(insertStatus).toBe(201);
 
     // Retrieve data
-    const {
-      data: selectData,
-      error: selectError,
-      status: selectStatus,
-      statusText: selectStatusText,
-    } = await supabase
+    const { data: selectData, error: selectError } = await supabase
       .from("test_table")
       .select("*")
       .eq("email", "test@example.com");
 
     console.log("Select Error:", selectError);
     console.log("Select Data:", selectData);
-    console.log("Select Status:", selectStatus);
-    console.log("Select Status Text:", selectStatusText);
 
     expect(selectError).toBeNull();
     expect(selectData).not.toBeNull();
@@ -58,18 +46,12 @@ describe("Supabase Client Integration", () => {
     expect(selectData![0].email).toBe("test@example.com");
 
     // Clean up
-    const {
-      error: deleteError,
-      status: deleteStatus,
-      statusText: deleteStatusText,
-    } = await supabase
+    const { error: deleteError } = await supabase
       .from("test_table")
       .delete()
       .eq("email", "test@example.com");
 
     console.log("Delete Error:", deleteError);
-    console.log("Delete Status:", deleteStatus);
-    console.log("Delete Status Text:", deleteStatusText);
 
     expect(deleteError).toBeNull();
   }, 10000); // Increase timeout to 10 seconds
--- src/tests/supabaseClient.test.disabled.ts
+++ src/tests/supabaseClient.test.disabled.ts
@@ -13,9 +13,8 @@ jest.mock("@supabase/supabase-js", () => {
 
 describe("Supabase Client", () => {
   it("should create a Supabase client with the correct URL and key", () => {
-    const supabaseUrl = "https://ppcmxkdoxdaroveggkfr.supabase.co";
-    const supabaseKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwY214a2RveGRhcm92ZWdna2ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQzMjcwNjcsImV4cCI6MjA0OTkwMzA2N30.RV0wVWkdE_CUYliTYeGyLavyB3H-2WPM_7xz8GGjMQY";
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabaseKey = process.env.SUPABASE_KEY;
 
     expect(createClient).toHaveBeenCalledWith(supabaseUrl, supabaseKey);
   });
